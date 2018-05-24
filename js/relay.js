e54c8dbab54e04e578ee25f2ebb80b6e13638528
t-andreea <trif.andreea@hotmail.com>
2018-05-24 17:34:16 +0000

    relay route implemented

---
+++ js/relay.js
@@ -0,0 +1,75 @@
+var button_value = "";
+var connection = "http://localhost:5000";
+
+
+function init() {
+  
+  var http = new XMLHttpRequest();
+  var url = connection + "/water";
+  http.open("GET", url, true);
+
+  http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  http.onreadystatechange = function() {
+     if(http.readyState == 4 && http.status == 200) {
+         var a = JSON.parse(http.responseText);
+         if(a.status == 0) {
+   	    button_value = a.content;
+         }
+	 if(button_value == "off") {
+	    button_value = "on";
+         }
+         else {
+            button_value = "off";
+         }
+     }
+  }
+
+  http.send();
+}
+
+
+function updateLabel() {
+
+  var http = new XMLHttpRequest();
+  var url = connection + "/sensor";
+  http.open("GET", url, true);
+
+  http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  http.onreadystatechange = function() {
+     if(http.readyState == 4 && http.status == 200) {
+         var a = JSON.parse(http.responseText);
+	 document.getElementById("temp-hum").innerHTML = "Humidity: " + a.content.humidity + "  Temperature: " + a.content.temperature;
+     }
+  }
+
+  http.send();
+}
+
+
+function sendRequest() {  
+
+  var http = new XMLHttpRequest();
+  var url = connection + "/water";
+  var params = "action=" + button_value;
+  http.open("POST", url, true);
+
+  http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  http.onreadystatechange = function() {
+     if(http.readyState == 4 && http.status == 200) {
+         var a = JSON.parse(http.responseText);
+         if(a.status == 0) {
+   	    if(button_value == "off") {
+	       button_value = "on";
+            }
+	    else {
+   	       button_value = "off";
+	    }
+	 }
+     }
+  }
+
+  http.send(params);
+}
