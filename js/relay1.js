586a8c49523c096248cb96950bca1b810a2ab0cd
Andreea Trif <trif.andreea@hotmail.com>
2018-06-14 00:18:47 +0300

    new front-end version - partially implemented

--- js/relay1.js
+++ js/relay1.js
@@ -29,6 +29,46 @@ function init() {
 }
 
 
+function renderTime() {
+
+  var date = new Date();
+  var year = date.getYear();
+  var day = date.getDay();
+  var month = date.getMonth();
+  var daym = date.getDate();
+
+  var dayArray = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday", "Saturday");
+  var monthArray = new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+
+  if(year < 1000) {
+    year += 1900;
+  }
+
+  var currentTime = new Date();
+  var h = currentTime.getHours();
+  var m = currentTime.getMinutes();
+  var s = currentTime.getSeconds();
+
+  if(h == 24) {
+    h = 0;
+  }
+  if(h < 10) {
+    h = "0" + h;
+  }
+  if(m < 10) {
+    m = "0" + m;
+  }
+  if(s < 10) {
+    s = "0" + s;
+  }
+
+  document.getElementById("date").innerHTML = "" + dayArray[day] + ", " + daym + " " + monthArray[month] + " " + year;
+  document.getElementById("time").innerHTML = "" +  h + ":" + m + ":" + s;
+
+  setTimeout("renderTime()", 1000);
+}
+
+
 function updateLabel() {
 
   var http = new XMLHttpRequest();
@@ -40,8 +80,9 @@ function updateLabel() {
   http.onreadystatechange = function() {
      if(http.readyState == 4 && http.status == 200) {
          var a = JSON.parse(http.responseText);
-	 document.getElementById("temp-hum").innerHTML = "Humidity: " + a.content.humidity + "<br/>  Temperature: " + a.content.temperature;
-     }
+	 document.getElementById("temp").innerHTML = "Temperature: " + a.content.temperature + "˚C";
+   document.getElementById("hum").innerHTML = "Humidity: " + a.content.humidity + "%";
+    }
   }
 
   http.send();
